adcd421d526ec85e9ce3aaa2e1f8ddfc82b6f2d8
Ivan Casales <casalesivan@hotmail.com>
2022-05-17 16:28:02 -0300

    IncreaseVP void added

--- src/Library/Characters/Heroes/IHero.cs
+++ src/Library/Characters/Heroes/IHero.cs
@@ -3,6 +3,6 @@ namespace RoleplayGame
     // La interfaz se utilizar para la lista de heroes en los Encounters
     public interface IHero
     {
-        
+        public void IncreaseVP(int VPValue);
     }
 }
